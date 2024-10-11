f08b248a464b9ddb9e51a95222dc2a42df325f8e
Diegoc04 <diego.fercho.castellanos@gmail.com>
2024-10-11 16:30:35 -0500

    Configurando micorsoft entra id

---
+++ src/config/authConfig.js
@@ -0,0 +1,8 @@
+const authConfig = {
+    clientId: 'd0617a79-38ff-4e03-8bbf-a5bd98d2e700', // El ID de aplicación del frontend registrado en Microsoft Entra ID
+    authority: 'https://login.microsoftonline.com/4d4d334a-9797-4b89-b8e9-28946972b8f3', // Tenant
+    redirectUri: 'http://localhost:3000', // La URI de redirección configurada en el registro de la aplicación
+  };
+  
+  export default authConfig;
+  
