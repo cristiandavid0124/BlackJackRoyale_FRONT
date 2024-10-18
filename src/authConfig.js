867c1f8810dcb93235269f9e40b08f8130aae9de
Daniel Santiago Torres <RulosS290>
2024-10-18 11:46:51 -0500

    Login EntraID

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,17 +1,46 @@
-const msalConfig = {
+import { LogLevel } from '@azure/msal-browser';
+
+
+
+export const msalConfig = {
     auth: {
-        clientId: "a18be00e-d104-4272-b36c-7a05bee4eff4", // Tu client ID
-        authority: "https://login.microsoftonline.com/4d4d334a-9797-4b89-b8e9-28946972b8f3/v2.0", // Tu issuer-uri
-        redirectUri: "http://localhost:3000", // Cambia esto por la URL de tu aplicación en producción
+        clientId: '066fef70-45bd-422a-a870-149ae2f088f4', // This is the ONLY mandatory field that you need to supply.
+        authority: 'https://login.microsoftonline.com/common/v2.0', // Replace the placeholder with your tenant subdomain 
+        redirectUri: 'http://localhost:3000/', // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
+        postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
+        navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
     },
     cache: {
-        cacheLocation: "localStorage", // Puedes usar localStorage o sessionStorage
-        storeAuthStateInCookie: false, // true para IE11 o Edge
+        cacheLocation: 'sessionStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                    default:
+                        return;
+                }
+            },
+        },
     },
 };
 
-const loginRequest = {
-    scopes: ["openid", "profile", "email"],
+export const loginRequest = {
+    scopes: [],
 };
-
-export { msalConfig, loginRequest };
